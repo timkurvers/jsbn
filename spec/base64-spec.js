c3fe84c7fad44b66fc2b9a4e93e8606df3e94ab8
Tim Kurvers <tim@moonsphere.net>
2014-10-28 23:02:24 +0100

    Add base64 and its specs

---
+++ spec/base64-spec.js
@@ -0,0 +1,20 @@
+helper = require('./spec-helper');
+expect = helper.expect;
+
+describe('Base64', function() {
+
+  var base64 = require('../lib/base64');
+
+  it('can be generated from hex', function() {
+    expect(base64.fromHex('4a53424e')).to.eq('SlNCTg==');
+  });
+
+  it('can be converted to hex', function() {
+    expect(base64.toHex('SlNCTg==')).to.eq('4a53424e');
+  });
+
+  it('can be converted to byte-array', function() {
+    expect(base64.toByteArray('SlNCTg==')).to.eq([74, 83, 66, 78]);
+  });
+
+});
